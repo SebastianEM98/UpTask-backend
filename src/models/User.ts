2171ae463659e6c7f538ab44a550e13ca78f0ed2
Sebastian Echeverri <sebas.eche98@gmail.com>
2025-12-26 13:02:47 -0500

    User model created

---
+++ src/models/User.ts
@@ -0,0 +1,36 @@
+import { Schema, Document, model } from "mongoose"
+
+export interface IUser extends Document {
+    email: string,
+    password: string
+    name: string
+    confirmed: boolean
+}
+
+const UserSchema: Schema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    confirmed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+// Hides the __v in responses (remains on database)
+UserSchema.set("toJSON", { versionKey: false })
+UserSchema.set("toObject", { versionKey: false })
+
+const User = model<IUser>("User", UserSchema)
+export default User
